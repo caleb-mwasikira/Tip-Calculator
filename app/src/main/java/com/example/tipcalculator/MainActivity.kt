a5f738f8164a86285bf116a5e9480de26ac054da
caleb_mwasikira <calebmwasikira@gmail.com>
2023-04-28 01:18:36 +0300

    Implemented functionality for calculating tips

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tipResult.text = getString(R.string.tip_result, "0.00")
+        binding.calculateBtn.setOnClickListener{ calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val costOfService = stringInTextField.toDoubleOrNull()
+        if(costOfService == null) {
+            binding.tipResult.text = getString(R.string.tip_result, "0.00")
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+            R.id.amazing -> 0.20
+            R.id.good -> 0.18
+            else -> 0.15
+        }
+        var tip = costOfService * tipPercentage
+        if(binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_result, formattedTip)
     }
 }
